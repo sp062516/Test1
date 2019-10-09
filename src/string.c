4251d1f7533833a995c199636b875e0025fd17b6
JohnSklAdd <ex4786.john@gmail.com>
2019-10-09 14:06:54 +0800

    mv john_sample/ to src

--- src/string.c
+++ src/string.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-size_t my_strlen(char *s)
+size_t my_strlen(const char *s)
 {
 	char *p = s;
 	while (*p) ++p;
 	return (p - s);
 }
 
-char *my_strcpy(char *t, char *s)
+char *my_strcpy(const char *t, const char *s)
 {
 	char *p = t; 
 	while (*t ++ = *s ++);
